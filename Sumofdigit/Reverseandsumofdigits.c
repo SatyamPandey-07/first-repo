a6a046aade7429c9e796e19f2dc6568c9c847df5
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2024-10-27 10:32:39 +0530

    Update Reverseandsumofdigits.c

--- Sumofdigit/Reverseandsumofdigits.c
+++ Sumofdigit/Reverseandsumofdigits.c
@@ -1,1 +1,18 @@
-h
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+long long num,n,t=0, i=1, sum=0; printf("Enter number: ");
+ scanf("%d", &num); 
+n=num;
+while(n>=1)
+{ t=t*10; 
+t += (n%10); 
+n=n/10;  i++; }
+  
+while(num>=1)
+{ sum+=num%10; num=num/10; } 
+  printf("Reverse: %d\nSum of digits: %d", t,sum);
+ return 0;   
+}
