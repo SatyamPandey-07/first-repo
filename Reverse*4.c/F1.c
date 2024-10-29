31d7204f17cde17176dee0a9afd8d919f883678c
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2024-10-29 09:54:47 +0530

    Update F1.c

--- Reverse*4.c/F1.c
+++ Reverse*4.c/F1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int reverse(int n) {
+    int rev = 0;
+    while (n != 0) {
+        rev = rev * 10 + (n % 10);
+        n /= 10;
+    }
+    return rev;
+}
+
+int main() {
+    for (int i = 10000; i < 99999; i++) {
+        if (i == 4 * reverse(i)) {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
 
