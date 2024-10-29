91bcd81e2320baebe65a531115e42070fa00e386
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2024-10-29 09:43:49 +0530

    Update 100 to 9999.c

--- Armstrong no.c/100 to 9999.c
+++ Armstrong no.c/100 to 9999.c
@@ -1,1 +1,24 @@
+#include <math.h>
 
+int nodigits(int n) {
+    int count = 0;
+    while (n > 0) {
+        count++;
+        n /= 10;
+    }
+    return count;
+}
+
+int main() {
+    for (int i = 100; i <= 9999; i++) {
+        int ti = i, sum = 0;
+        while (ti > 0) {
+            sum += pow(ti % 10, nodigits(i));
+            ti /= 10;
+        }
+        if (sum == i) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
