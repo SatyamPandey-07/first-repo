e968f204d98902f27b85c4a1f070482b8d9578a9
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2024-10-30 13:47:19 +0530

    Update Input.c

--- Collatz conjecture logic/Input.c
+++ Collatz conjecture logic/Input.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+
+    // Input a number
+    scanf("%d", &n);
+
+    // Collatz Conjecture loop
+    while (n != 1) {
+        if (n % 2) 
+            n = 3 * n + 1;
+        else 
+            n = n / 2;
+        
+        printf("%d ", n);
+        count++;
+    }
+
+    // Print the number of steps
+    printf("\nNumber of steps: %d", count);
+
+    return 0;
+}
