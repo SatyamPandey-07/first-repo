8aa325e919498559d7eef1807a9e680acb594281
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2024-10-30 13:57:21 +0530

    Update First 40 no.c

--- Fibonacci series/First 40 no.c
+++ Fibonacci series/First 40 no.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int main() {
+    int arr[40];
+
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i = 2; i < 40; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    printf("First 40 Fibonacci numbers are:\n");
+    for (int i = 0; i < 40; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
