15ee8dba5f58b28414a81216c39441c59f2fd4b6
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2024-11-06 10:01:36 +0530

    Create pow.c

---
+++ Tgamm/pow.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x, y;
+    double sum = 0.0;
+
+    printf("Enter a number: ");
+    scanf("%d", &x);
+
+    printf("Enter number of terms: ");
+    scanf("%d", &y);
+
+    // Calculate the sum of series
+    for (int i = 1; i <= y; i++) {
+        sum += pow(x, i) / tgamma(i + 1); // tgamma(i + 1) = factorial(i)
+    }
+
+    printf("Sum = %.2f\n", sum);
+    return 0;
+}
