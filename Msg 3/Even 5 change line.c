58bdf63224c28374dd8c68f1fc245dc37b01fbe1
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2024-11-11 16:58:52 +0530

    Update Even 5 change line.c

--- Msg 3/Even 5 change line.c
+++ Msg 3/Even 5 change line.c
@@ -3,4 +3,16 @@
 int main() {
     int count = 0;
     
-    for (int num =
+    for (int num = 2; num <= 100; num += 2) {
+        if (!(num % 8 == 0 && num % 10 == 0)) {
+            printf("%3d ", num);
+            count++;
+            
+            if (count % 5 == 0) {
+                printf("\n");  // Move to the next line after 5 numbers
+            }
+        }
+    }
+
+    return 0;
+}
