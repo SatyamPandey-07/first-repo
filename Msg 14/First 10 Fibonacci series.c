8109f9670488d59667efdc6c0b1058f318d82952
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2024-11-13 15:53:00 +0530

    Create First 10 Fibonacci series.c

---
+++ Msg 14/First 10 Fibonacci series.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n1 = 0, n2 = 1, nextTerm;
+
+    printf("Fibonacci Series: %d %d ", n1, n2);
+
+    for (int i = 3; i <= 10; i++) {
+        nextTerm = n1 + n2;
+        printf("%d ", nextTerm);
+        n1 = n2;
+        n2 = nextTerm;
+    }
+
+    return 0;
+}
